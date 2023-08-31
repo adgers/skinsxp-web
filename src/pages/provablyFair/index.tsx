051820b7da9c4aab9afbe5d67bc99b24ed711218
wupt <wupengtao@akey.me>
2023-08-31 17:45:06 +0800

    fix: 增加声音

---
+++ src/pages/provablyFair/index.tsx
@@ -0,0 +1,73 @@
+import { IconFont } from '@/components/icons';
+import { goback } from '@/utils';
+import { LeftOutlined } from '@ant-design/icons';
+import {
+  FormattedMessage,
+  Link,
+  Outlet,
+  useIntl,
+  useLocation,
+} from '@umijs/max';
+import { useMemo } from 'react';
+
+export default function ProvablyFair() {
+  const location = useLocation();
+  const intl = useIntl();
+  const tabLinks = useMemo(() => {
+    return [
+      {
+        title: <FormattedMessage id="provably_fair_config" />,
+        link: '/provably-fair/config',
+      },
+      {
+        title: <FormattedMessage id="provably_fair_verify" />,
+        link: '/provably-fair/verify',
+      },
+    ];
+  }, []);
+
+  return (
+    <div className="w-full max-w-[1400px] m-auto">
+      <div className="my-5 flex w-full flex-col justify-between border-b border-light lg:mb-0 lg:mt-8 lg:flex-row">
+        <div className="-mb-px items-center border-b border-green pb-6 pr-6 font-semibold uppercase text-white flex gap-3">
+          <div className="-my-2 text-white cursor-pointer" onClick={goback}>
+            <LeftOutlined />
+          </div>
+          <FormattedMessage id="provably_fair_title" />
+          <IconFont type="icon-shield" className="text-green text-2xl" />
+        </div>
+      </div>
+      <div className="bg-black rounded p-5 text-white mt-8">
+        <div
+          dangerouslySetInnerHTML={{
+            __html: intl.formatMessage({ id: 'provably_fair_content' }),
+          }}
+          className="prose-base prose-slate"
+        ></div>
+      </div>
+      <div className="custom-tab flex mt-4 text-white justify-center border-b border-[#45444B] w-full h-[68px]">
+        {tabLinks.map((item, index) => {
+          const isActive = location.pathname.startsWith(item.link);
+
+          return (
+            <div
+              className={`tab-item uppercase font-semibold px-4 flex items-center h-full ${
+                isActive
+                  ? 'text-green border-b-[1px] border-green'
+                  : 'text-white '
+              }`}
+              key={index}
+            >
+              <Link to={item.link} className="tab-item-c text-sm md:text-base">
+                {item.title}
+              </Link>
+            </div>
+          );
+        })}
+      </div>
+      <div className="w-full">
+        <Outlet />
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/provablyFair/serverSeedHistory.tsx
@@ -0,0 +1,97 @@
+import { keyHistoryUsingGET } from '@/services/front/miyaozhongzixiangguan';
+import { useRequest } from '@umijs/max';
+import { Pagination } from 'antd';
+import { useState } from 'react';
+import { Button, Modal } from 'react-daisyui';
+
+export default function ServerSeedHistory({
+  open,
+  onClose,
+}: {
+  open: boolean;
+  onClose: () => void;
+}) {
+  const [page, setPage] = useState(1);
+  const pageSize = 3;
+
+  const { data } = useRequest(
+    () => {
+      return keyHistoryUsingGET({
+        page: page,
+        pageSize: pageSize,
+      });
+    },
+    {
+      refreshDeps: [page],
+    },
+  );
+  return (
+    <Modal open={open} className="max-w-lg">
+      <Modal.Header className="uppercase font-semibold leading-tight">
+      Server Seed History
+      </Modal.Header>
+      <Button
+        size="xs"
+        shape="circle"
+        color="ghost"
+        className="absolute right-2 top-2"
+        onClick={onClose}
+      >
+        ✕
+      </Button>
+      <Modal.Body className="grid gap-y-6">
+        {data?.pageData?.map((item) => {
+          return (
+            <dl
+              className="grid gap-y-px overflow-hidden break-all rounded-md text-xs leading-tight grid-cols-[auto_1fr]"
+              key={item.id}
+            >
+              <dt className="whitespace-nowrap bg-black p-3 font-bold">
+                SERVER SEED
+              </dt>
+              <dd className="bg-base-100 bg-opacity-30 p-3">
+                {item.secretHash}
+              </dd>
+              <dt className="whitespace-nowrap bg-black p-3 font-bold ">
+                SECRET SALT
+              </dt>
+              <dd className="bg-base-100 bg-opacity-30 p-3">
+                {item.secretSalt}
+              </dd>
+              <dt className="whitespace-nowrap bg-black p-3 font-bold ">
+                PUBLIC HASH
+              </dt>
+              <dd className="bg-base-100 bg-opacity-30 p-3">
+                {item.publicHash}
+              </dd>
+              <dt className="whitespace-nowrap bg-black p-3 font-bold ">
+                ROUND
+              </dt>
+              <dd className="bg-base-100 bg-opacity-30 p-3">{item.round}</dd>
+              <dt className="whitespace-nowrap bg-black p-3 font-bold ">
+                DATE
+              </dt>
+              <dd className="bg-base-100 bg-opacity-30 p-3">
+                {item.createTime}
+              </dd>
+            </dl>
+          );
+        })}
+
+        {data?.totalRows && data?.totalRows > pageSize && (
+          <div className="flex justify-center items-center mt-2">
+            <Pagination
+              current={page}
+              total={data?.totalRows}
+              pageSize={pageSize}
+              showSizeChanger={false}
+              onChange={(page: number) => {
+                setPage(page);
+              }}
+            />
+          </div>
+        )}
+      </Modal.Body>
+    </Modal>
+  );
+}
