8eed2c1167a1e47e2d2a20d2e110ccdc8e27e3b5
wupt <wupengtao@akey.me>
2023-09-10 16:33:18 +0800

    fix

--- src/pages/battle/endRooms.tsx
+++ src/pages/battle/endRooms.tsx
@@ -4,7 +4,7 @@ import { getPageUsingGET } from '@/services/front/duizhanxiangguan';
 import { LoadingOutlined } from '@ant-design/icons';
 import { history, useModel, useRequest } from '@umijs/max';
 import { Pagination, Spin } from 'antd';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 export default function EndRooms({
   show,
@@ -15,16 +15,9 @@ export default function EndRooms({
   mode: number;
   pageSize?: number;
 }) {
-  const { battleState } = useModel('socket');
-  const [rooms, setRooms] = useState<API.BattleVo[]>();
   const [page, setPage] = useState(1);
-  const [total, setTotal] = useState(0);
 
-  const {
-    data: endBattles,
-    refresh,
-    loading,
-  } = useRequest(
+  const { data: endBattles, loading } = useRequest(
     () => show && getPageUsingGET({ pageSize, page, state: 2, mode }),
     {
       refreshDeps: [show, page, mode],
@@ -32,31 +25,15 @@ export default function EndRooms({
     },
   );
 
-  useEffect(() => {
-    if (!show) {
-      return;
-    }
-
-    if (endBattles && endBattles?.pageData) {
-      setRooms(endBattles.pageData);
-      setTotal(endBattles.totalRows || 0);
-    }
-    if (battleState) {
-      if (battleState.state === 2) {
-        refresh();
-      }
-    }
-  }, [endBattles, battleState, show]);
-
   return (
     <Spin
       spinning={loading}
       indicator={<LoadingOutlined style={{ fontSize: 48, color: 'green' }} />}
     >
-      <div className="flex-1 items-center flex flex-col w-full relative min-h-[300px]" >
-        {rooms?.length === 0 && <Empty />}
+      <div className="flex-1 items-center flex flex-col w-full relative min-h-[300px]">
+        {endBattles?.pageData?.length === 0 && <Empty />}
         <div className="w-full flex flex-col gap-2">
-          {rooms?.map((t) => (
+          {endBattles?.pageData?.map((t) => (
             <RoomCard
               key={t.battleCode}
               data={t}
@@ -66,11 +43,11 @@ export default function EndRooms({
             />
           ))}
         </div>
-        {total > pageSize && (
+        {!!endBattles?.totalRows && endBattles?.totalRows > pageSize && (
           <div className="flex justify-center items-center mt-8">
             <Pagination
               current={page}
-              total={total}
+              total={endBattles.totalRows}
               pageSize={12}
               showSizeChanger={false}
               onChange={(page: number) => {
