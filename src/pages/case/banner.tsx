f49cbdebd44931122ad1edacd53e33e8eead1d8b
nibaoshi <baoshi@1902434007321951.onaliyun.com>
2023-09-26 21:46:12 +0800

    up

--- src/pages/case/banner.tsx
+++ src/pages/case/banner.tsx
@@ -60,12 +60,11 @@ export default function Banner() {
               className="w-full h-full object-cover"
             />
             <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center z-30">
-              <div className="text-lg sm:text-[50px] sm:mb-10 text-center uppercase">
-                {
-                  intl
-                    .formatMessage({ id: 'banner_first_recharge' })
-                    ?.split('\\n')[0]
-                }
+              <div className="sm:text-[40px] sm:mb-10 text-center uppercase">
+                {intl.formatMessage({ id: 'banner_first_recharge' })} &
+              </div>
+              <div className="sm:text-[40px] sm:mb-10 text-center uppercase">
+                {intl.formatMessage({ id: 'banner_promo' })}
               </div>
               <div
                 className="text-primary text-xl sm:h-[60px] sm:leading-none sm:text-[60px] font-semibold uppercase"
@@ -77,51 +76,7 @@ export default function Banner() {
                   color: 'transparent',
                 }}
               >
-                {
-                  intl
-                    .formatMessage({ id: 'banner_first_recharge' })
-                    ?.split('\\n')[1]
-                }
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide
-            key={1}
-            className="h-[125px] sm:h-[200px] md:h-[250px] lg:h-[300px] xl:h-[350px] xxl:h-[400px] relative"
-            onClick={() => {
-              history.push({
-                pathname: '/deposit',
-                search: location.search,
-              });
-              window?.fbq(
-                'trackSingleCustom',
-                '1024868335308144',
-                'InitiateCheckout',
-              );
-              window?.gtag('event', 'conversion', {
-                send_to: 'AW-11345409756/EOFKCIqQy-UYENzt9KEq',
-              });
-            }}
-          >
-            <img
-              src={require('@/assets/banner-bg.png')}
-              className="w-full h-full object-cover"
-            />
-            <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center z-30">
-              <div className="text-lg sm:text-[50px] sm:mb-10 text-center uppercase">
-                {intl.formatMessage({ id: 'banner_promo' })?.split('\\n')[0]}
-              </div>
-              <div
-                className=" text-xl sm:h-[60px] sm:text-[60px] sm:leading-none font-semibold uppercase"
-                style={{
-                  backgroundImage:
-                    'linear-gradient(270deg, #0BFF59 0.04%, #B4FC3B 99.77%)',
-                  WebkitBackgroundClip: 'text',
-                  overflow: 'visible',
-                  color: 'transparent',
-                }}
-              >
-                {intl.formatMessage({ id: 'banner_promo' })?.split('\\n')[1]}
+                {intl.formatMessage({ id: 'banner_bonus' })}
               </div>
             </div>
           </SwiperSlide>
--- src/pages/login/loginTip.tsx
+++ src/pages/login/loginTip.tsx
@@ -9,7 +9,7 @@ export default function LoginTip() {
   return (
     <Modal
       open={loginTipShow}
-      className="w-[300px] h-[340px] border-none p-0 overflow-hidden rounded-md"
+      className="w-[300px] h-[340px] sm:w-[450px] sm:h-[510px] border-none p-0 overflow-hidden rounded-md"
     >
       <Button
         size="xs"
@@ -21,7 +21,7 @@ export default function LoginTip() {
         ✕
       </Button>
       <Modal.Body
-        className="flex h-full flex-col items-center px-8 py-6 bg-no-repeat  bg-cover "
+        className="flex h-full flex-col items-center px-8 py-6 sm:py-10 bg-no-repeat  bg-cover "
         style={{ backgroundImage: `url(${bgImg})` }}
       >
         <div className="w-full h-full relative flex flex-col items-center uppercase">
@@ -64,11 +64,11 @@ export default function LoginTip() {
               </div>
             </div>
           </div>
-          <div className="absolute top-[128px] w-[50px] left-0 font-semibold text-right">
+          <div className="absolute top-[128px] sm:top-[189px] w-[50px] sm:w-[75px] left-0 sm:left-[10px] font-semibold text-right sm:text-xl">
             <FormattedMessage id="loginTip_up" />
           </div>
           <div
-            className="absolute top-[114px] right-0 w-[100px] font-bold text-center"
+            className="absolute top-[114px] sm:top-[174px] right-0 sm:right-[10px] w-[100px] sm:w-[160px] font-bold text-center"
             style={{
               backgroundImage:
                 'linear-gradient(270deg, #FFB1B6 0%, #FFEEF4 43.4%, #FFC8DE 93.42%)',
@@ -81,7 +81,7 @@ export default function LoginTip() {
           </div>
 
           <Button
-            className="btn btn-purple  absolute m-auto bottom-0 w-[50%] md:w-[60%] "
+            className="btn btn-purple absolute m-auto bottom-0  "
             onClick={() => {
               hideLoginTip();
               showSteamLogin();
