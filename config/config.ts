ea8d2b1755991aec25e32c39691dd474b991579a
nibaoshi <baoshi@1902434007321951.onaliyun.com>
2023-10-17 15:52:46 +0800

    fix:更换ga id

--- config/config.ts
+++ config/config.ts
@@ -239,7 +239,7 @@ export default defineConfig({
       gtag('config', 'AW-11366921880');
       gtag('config', 'AW-11345409756');
       gtag('config', 'AW-11366618499');
-      gtag('config', 'G-6LDWG1BWSC');
+      gtag('config', 'G-Y7T3FTBJYT');
       `,
     },
   ],
