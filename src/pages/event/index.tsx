17c5ac63d112f73eb28618cba57dffa5e6d5f619
nibaoshi <baoshi@1902434007321951.onaliyun.com>
2023-10-25 14:57:47 +0800

    feat:halloween zancun

--- src/pages/event/index.tsx
+++ src/pages/event/index.tsx
@@ -3,7 +3,7 @@ export default () => {
   return (
     <div className="w-full max-w-[1400px] m-auto relative min-h-[500px]">
       <Banner />
-      <div className='flex max-w-3xl border border-purple py-[30px] mx-auto translate-y-[-50px] ' style={{background: 'var(--C3, radial-gradient(124.94% 91.38% at 49.87% 100%, #533476 0%, #1F1A33 100%))'}}>
+      <div className='flex max-w-5xl border border-purple py-[30px] mx-auto translate-y-[-50px] ' style={{background: 'var(--C3, radial-gradient(124.94% 91.38% at 49.87% 100%, #533476 0%, #1F1A33 100%))'}}>
         <div className='w-full flex flex-col items-center justify-center'>
           <div>FIRST DEPOSIT</div>
           <span>Enter the code “helloween” +20%</span>
