17fe80f0c9eb5f626c307122f69786149c448d51
nibaoshi <baoshi@1902434007321951.onaliyun.com>
2023-10-25 16:48:17 +0800

    暂存

--- src/pages/event/index.tsx
+++ src/pages/event/index.tsx
@@ -1,15 +1,25 @@
+import { taskListUsingGET } from '@/services/front/huodongzhongxinxiangguan';
+import { useRequest } from '@umijs/max';
 import Banner from '../case/banner';
 export default () => {
+  const { data, loading, refresh } = useRequest(() => taskListUsingGET());
+  console.log(data,'data')
   return (
     <div className="w-full max-w-[1400px] m-auto relative min-h-[500px]">
       <Banner />
-      <div className='flex max-w-5xl border border-purple py-[30px] mx-auto translate-y-[-50px] ' style={{background: 'var(--C3, radial-gradient(124.94% 91.38% at 49.87% 100%, #533476 0%, #1F1A33 100%))'}}>
-        <div className='w-full flex flex-col items-center justify-center'>
+      <div
+        className="flex max-w-5xl border border-purple py-[30px] mx-auto translate-y-[-50px] "
+        style={{
+          background:
+            'var(--C3, radial-gradient(124.94% 91.38% at 49.87% 100%, #533476 0%, #1F1A33 100%))',
+        }}
+      >
+        <div className="w-full flex flex-col items-center justify-center">
           <div>FIRST DEPOSIT</div>
           <span>Enter the code “helloween” +20%</span>
           <button className="btn btn-purple">Refil</button>
         </div>
-        <div className='w-full flex items-center justify-center'>
+        <div className="w-full flex items-center justify-center">
           <div>Free roll</div>
           <div>
             <span>12/279</span>
@@ -17,7 +27,9 @@ export default () => {
           </div>
         </div>
       </div>
-      <div className='text-xl font-bold text-center'>HALLOWEEN LIMITED TIMEEVENT</div>
+      <div className="text-xl font-bold text-center">
+        HALLOWEEN LIMITED TIMEEVENT
+      </div>
     </div>
   );
 };
