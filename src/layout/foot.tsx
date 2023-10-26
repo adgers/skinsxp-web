d46ae841d690803753f02d047ba87efedbf85fdf
nibaoshi <baoshi@1902434007321951.onaliyun.com>
2023-10-26 22:32:17 +0800

    feat：去掉网站数据

--- src/layout/foot.tsx
+++ src/layout/foot.tsx
@@ -116,7 +116,7 @@ export default function Foot() {
         </div>
       )}
       <footer className="bg-base-300 text-base-content mt-10 flex flex-col">
-        <div className="bg-black w-full flex justify-center">
+        {/* <div className="bg-black w-full flex justify-center">
           <div className="max-w-8xl w-full relative grid grid-cols-2 gap-y-4 sm:grid-cols-5 text-gray py-10">
             {onlineStatMap.map((item, index) => {
               return (
@@ -135,7 +135,7 @@ export default function Foot() {
               );
             })}
           </div>
-        </div>
+        </div> */}
         <div className="max-w-8xl w-full m-auto relative">
           <div className="grid grid-cols-2 gap-y-4 sm:grid-cols-5 text-gray py-10">
             {footerTop.map((item, index) => {
