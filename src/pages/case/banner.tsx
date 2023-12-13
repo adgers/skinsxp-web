f0f8e5821f7f7601693ff00f15463b524d8450a7
nibaoshi <baoshi@1902434007321951.onaliyun.com>
2023-12-13 19:03:27 +0800

    feat:xmas

--- src/pages/case/banner.tsx
+++ src/pages/case/banner.tsx
@@ -1,10 +1,16 @@
-import { useIntl } from '@umijs/max';
+import { FormattedMessage, history, useIntl, useLocation } from '@umijs/max';
+import { useCountDown } from 'ahooks';
+import { Countdown } from 'react-daisyui';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import { Autoplay, Pagination } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 export default function Banner() {
+  const [countdown, formattedRes] = useCountDown({ targetDate: 1703951999000 });
+  const { days, hours, minutes, seconds } = formattedRes;
+  const location = useLocation();
+
   const intl = useIntl();
   // const { data, loading } = useRequest(
   //   () => getBannerListUsingGET({ topN: 10 }),
@@ -28,45 +34,112 @@ export default function Banner() {
         modules={[Pagination, Autoplay]}
         className="w-full rounded h-[180px] md:h-[350px] lg:h-[350px] xl:h-[400px] 2xl:h-[450px] 1080p:h-[600px] 4k:h-[880px] flex items-center !z-0"
       >
-        {/* {data?.map((t, i) => ( */}
         <SwiperSlide
           key={0}
           className="h-[150px]  md:h-[350px] lg:h-[350px] xl:h-[400px] 2xl:h-[450px] 1080p:h-[600px] 4k:h-[880px] relative"
-          onClick={() => {
-            window.open('https://t.me/+nLl1Wg9toBhlNjc5');
-          }}
+          onClick={() => {}}
         >
           <img
-            src={require('@/assets/banner-steam.png')}
+            src={require('@/assets/xmas-banner.png')}
             className="w-full h-full object-cover cursor-pointer"
           />
-        </SwiperSlide>
-        {/* ))} */}
-        <SwiperSlide
-          key={1}
-          className="h-[150px]  md:h-[350px] lg:h-[350px] xl:h-[400px] 2xl:h-[450px] 1080p:h-[600px] 4k:h-[880px] relative"
-          onClick={() => {
-            window.open('https://discord.gg/ucDMTAbAcS');
-          }}
-        >
-          <img
-            src={require('@/assets/banner-discord.png')}
-            className="w-full h-full object-cover"
-          />
-          <div className="absolute top-0 left-0 w-full h-full flex flex-col items-center z-30">
-            <div
-              className="text-[16px] md:text-[50px] text-center uppercase font-num font-semibold mt-5 md:mt-10 1080p:mt-40 w-[160px] md:w-[800px] whitespace-pre-wrap"
-              style={{ fontFamily: 'Bebas' }}
-            >
-              {intl.formatMessage({ id: 'banner_discord_text' })}
+          <div className="absolute top-0 left-0 w-full h-full flex flex-col  items-center z-30">
+            <div className="w-fit mt-[60px] md:mt-[140px] 1080p:mt-[200px] mx-auto flex gap-2">
+              {days > 0 && (
+                <div className="flex w-[60px] md:w-[66px] 4k:w-[150px] h-[36px] md:h-[80px] 4k:h-[200px] flex-shrink-0 flex-col items-center justify-center text-center bg-[#4B557A]/60 rounded">
+                  <Countdown
+                    value={days}
+                    className="days-value text-sm md:text-lg font-semibold tabular-nums 4k:text-[48px] leading-none text-white"
+                  />
+                  <span className="transform scale-75 font-medium leading-none text-white 4k:text-[32px] text-opacity-70">
+                    <FormattedMessage id="text_time_days" />
+                  </span>
+                </div>
+              )}
+              <div className="flex w-[60px] md:w-[66px] 4k:w-[150px] h-[36px] md:h-[80px] 4k:h-[200px] flex-shrink-0 flex-col items-center justify-center text-center bg-[#4B557A]/60 rounded">
+                <Countdown
+                  value={hours}
+                  className="days-value text-sm md:text-lg font-semibold tabular-nums 4k:text-[48px] leading-none text-white"
+                />
+                <span className="transform scale-75 font-medium leading-none text-white 4k:text-[32px] text-opacity-70">
+                  <FormattedMessage id="text_time_hour" />
+                </span>
+              </div>
+              <div className="flex w-[60px] md:w-[66px] 4k:w-[150px] h-[36px] md:h-[80px] 4k:h-[200px] flex-shrink-0 flex-col items-center justify-center text-center bg-[#4B557A]/60 rounded">
+                <Countdown
+                  value={minutes}
+                  className="days-value text-sm md:text-lg font-semibold tabular-nums 4k:text-[48px] leading-none text-white"
+                />
+                <span className="transform scale-75 font-medium leading-none text-white 4k:text-[32px] text-opacity-70">
+                  <FormattedMessage id="text_time_min" />
+                </span>
+              </div>
+
+              <div className="flex w-[60px] md:w-[66px] 4k:w-[150px] h-[36px] md:h-[80px] 4k:h-[200px] flex-shrink-0 flex-col items-center justify-center text-center bg-[#4B557A]/60 rounded">
+                <Countdown
+                  value={seconds}
+                  className="days-value text-sm md:text-lg font-semibold tabular-nums 4k:text-[48px] leading-none text-white"
+                />
+                <span className="transform scale-75 font-medium leading-none text-white 4k:text-[32px] text-opacity-70">
+                  <FormattedMessage id="text_time_sec" />
+                </span>
+              </div>
             </div>
-            <div className="w-[120px] h-[36px] md:w-[310px] md:h-[95px] bg-[url('@/assets/banner-discord-btn.png')] bg-no-repeat bg-cover mt-2 lg:mt-[60px] text-xs md:text-xl  md:py-10 flex items-center justify-center cursor-pointer">
-              <span className="pl-8">
-                {intl.formatMessage({ id: 'banner_discord_btn' })}
-              </span>
-            </div>
+            {location.pathname === '/case' && (
+              <div
+                className="btn !bg-[#633306]/80 !border-yellow hover:!bg-[#633306]/50 text-white !h-[32px] 4k:!h-[96px] 4k:text-[32px] md:!h-12 min-h-0 mt-3 md:mt-8 !px-8"
+                onClick={() => {
+                  history.push('/event');
+                }}
+              >
+                <FormattedMessage id="go_event" />
+              </div>
+            )}
           </div>
         </SwiperSlide>
+
+        {location.pathname === '/case' && (
+          <>
+            <SwiperSlide
+              key={1}
+              className="h-[150px]  md:h-[350px] lg:h-[350px] xl:h-[400px] 2xl:h-[450px] 1080p:h-[600px] 4k:h-[880px] relative"
+              onClick={() => {
+                window.open('https://t.me/+nLl1Wg9toBhlNjc5');
+              }}
+            >
+              <img
+                src={require('@/assets/banner-steam.png')}
+                className="w-full h-full object-cover cursor-pointer"
+              />
+            </SwiperSlide>
+
+            <SwiperSlide
+              key={2}
+              className="h-[150px]  md:h-[350px] lg:h-[350px] xl:h-[400px] 2xl:h-[450px] 1080p:h-[600px] 4k:h-[880px] relative"
+              onClick={() => {
+                window.open('https://discord.gg/ucDMTAbAcS');
+              }}
+            >
+              <img
+                src={require('@/assets/banner-discord.png')}
+                className="w-full h-full object-cover"
+              />
+              <div className="absolute top-0 left-0 w-full h-full flex flex-col items-center z-30">
+                <div
+                  className="text-[16px] md:text-[50px] text-center uppercase font-num font-semibold mt-5 md:mt-10 1080p:mt-40 w-[160px] md:w-[800px] whitespace-pre-wrap"
+                  style={{ fontFamily: 'Bebas' }}
+                >
+                  {intl.formatMessage({ id: 'banner_discord_text' })}
+                </div>
+                <div className="w-[120px] h-[36px] md:w-[310px] md:h-[95px] bg-[url('@/assets/banner-discord-btn.png')] bg-no-repeat bg-cover mt-2 lg:mt-[60px] text-xs md:text-xl  md:py-10 flex items-center justify-center cursor-pointer">
+                  <span className="pl-8">
+                    {intl.formatMessage({ id: 'banner_discord_btn' })}
+                  </span>
+                </div>
+              </div>
+            </SwiperSlide>
+          </>
+        )}
       </Swiper>
       {/* )} */}
     </div>
--- src/pages/event/index.tsx
+++ src/pages/event/index.tsx
@@ -1,23 +1,15 @@
 import '@/pages/case/index.less';
-import { listHostGiveawayUsingGET } from '@/services/front/ROLLfangxiangguan';
 import {
   completeUsingPOST,
   rewardUsingPOST,
   taskListUsingGET,
 } from '@/services/front/huodongzhongxinxiangguan';
 import { getPromotionInfoUsingGET } from '@/services/front/tuiguangzhongxinxiangguan';
-import { DownOutlined, RightOutlined, UpOutlined } from '@ant-design/icons';
-import {
-  FormattedMessage,
-  history,
-  useIntl,
-  useModel,
-  useRequest,
-} from '@umijs/max';
+import { DownOutlined, UpOutlined } from '@ant-design/icons';
+import { FormattedMessage, useIntl, useModel, useRequest } from '@umijs/max';
 import { useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import GiveawayItem from '../case/giveawayItem';
 import './index.less';
 
 enum CycleMode {
@@ -39,9 +31,6 @@ enum TaskEvent {
 export default () => {
   const intl = useIntl();
   const [show, setShow] = useState(false);
-  const { data: giveawayList } = useRequest(() => listHostGiveawayUsingGET(), {
-    cacheKey: 'giveawayList',
-  });
   const { data, loading, refresh } = useRequest(() => taskListUsingGET());
   const { data: promotionData = {} } = useRequest(() =>
     getPromotionInfoUsingGET(),
@@ -110,205 +99,54 @@ export default () => {
   };
   return (
     <div className="w-full max-w-[1400px] m-auto relative min-h-[500px]">
-      <div className="mx-auto max-w-[80%]  md:max-w-5xl translate-y-[-50px] flex snap-x snap-mandatory overflow-x-auto hide-scrollbar xl:grid xl:snap-none xl:grid-cols-4 opacity-100 gap-4">
-        {giveawayList?.map((item, index) => {
-          return <GiveawayItem index={index} item={item} key={index} />;
-        })}
-      </div>
-      <div className="mx-auto max-w-[80%]  md:max-w-5xl">
-        <div
-          className="w-full flex items-center md:items-start gap-2 md:gap-0 border border-red"
-          style={{
-            background:
-              'var(--E2, linear-gradient(90deg, rgba(150, 21, 44, 0.54) 0%, rgba(125, 25, 31, 0.00) 50%), rgba(22, 22, 22, 0.75))',
-          }}
-        >
-          <div className=" w-full flex flex-col items-center justify-center gap-2  py-2 md:py-[30px] bg-[url('@/assets/blackfriday-bg.png')] bg-no-repeat bg-contain">
-            <div className="flex gap-2 text-sm md:text-lg font-bold items-center">
-              <img
-                src={require('@/assets/blackfriday-tag.png')}
-                className=" w-5 md:w-8"
-              />{' '}
-              <FormattedMessage id="black_event_partner" />
-            </div>
-            <div
-              className="rounded bg-red text-white font-bold uppercase cursor-pointer px-8 py-2"
-              onClick={() => {
-                history.push('/profile/partner');
-              }}
-            >
-              <FormattedMessage id="black_event_invite" /> <RightOutlined />
-            </div>
-          </div>
-          <span className="w-[1px] h-[80px] bg-light mt-2 md:mt-[30px]"></span>
-          <div className="w-full flex flex-col md:flex-row items-center justify-center gap-0 md:gap-10  py-2 md:py-[30px] ">
-            <div className="flex flex-col text-gray text-sm text-center gap-2">
-              <div>
-                <FormattedMessage id="promoteCode_user_num" />
-              </div>
-              <div className="text-white text-lg">
-                {promotionData?.accumulatedRegisterCount}
-              </div>
-            </div>
-            <div className="flex flex-col items-center gap-2">
-              <div className="text-gray text-sm">
-                <FormattedMessage id="promoteCode_rebate_num" />
-              </div>
-              <div className="text-yellow text-lg">
-                ${promotionData?.accumulatedRebateAmount}
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="w-full  md:max-w-5xl items-center mx-auto justify-center mt-4">
-          <div
-            className="text-center text-gray cursor-pointer"
-            onClick={() => {
-              setShow(!show);
-            }}
-          >
-            <FormattedMessage id="black_event_zmz" />{' '}
-            {show ? <UpOutlined /> : <DownOutlined />}
-          </div>
-          <div
-            className={`w-full overflow-y-clip mt-2  ${show ? 'show' : 'hide'}`}
-          >
-            <div className="hidden grid-cols-3 gap-12 lg:grid">
-              <div
-                className="relative flex items-center h-full rounded-lg  px-6 py-2 text-sm"
-                style={{
-                  background:
-                    'linear-gradient(90deg, #453A3A 0%, rgba(56, 45, 46, 0.00) 100%)',
-                }}
-              >
-                <FormattedMessage id="black_event_work1" />
-              </div>
-              <div
-                className="relative flex items-center h-full rounded-lg  px-6 py-2 text-sm"
-                style={{
-                  background:
-                    'linear-gradient(90deg, #453A3A 0%, rgba(56, 45, 46, 0.00) 100%)',
-                }}
-              >
-                <FormattedMessage id="black_event_work2" />
-              </div>
-              <div
-                className="relative flex items-center h-full rounded-lg  px-6 py-2 text-sm"
-                style={{
-                  background:
-                    'linear-gradient(90deg, #453A3A 0%, rgba(56, 45, 46, 0.00) 100%)',
-                }}
-              >
-                <FormattedMessage id="black_event_work3" />
-              </div>
-            </div>
+      <div className="mx-auto max-w-[80%]  md:max-w-5xl"></div>
 
-            <Swiper className="block lg:!hidden">
-              <SwiperSlide>
-                <div className="swiper-slide swiper-slide-active">
-                  <div
-                    className="relative flex items-center h-full rounded-lg  px-6 py-2 text-sm"
-                    style={{
-                      background:
-                        'linear-gradient(90deg, #453A3A 0%, rgba(56, 45, 46, 0.00) 100%)',
-                    }}
-                  >
-                    <FormattedMessage id="black_event_work1" />
-                  </div>
-                </div>
-              </SwiperSlide>
-              <SwiperSlide>
-                <div className="swiper-slide swiper-slide-next">
-                  <div
-                    className="relative flex items-center h-full rounded-lg  px-6 py-2 text-sm"
-                    style={{
-                      background:
-                        'linear-gradient(90deg, #453A3A 0%, rgba(56, 45, 46, 0.00) 100%)',
-                    }}
-                  >
-                    <FormattedMessage id="black_event_work2" />
-                  </div>
-                </div>
-              </SwiperSlide>
-              <SwiperSlide>
-                <div className="swiper-slide">
-                  <div
-                    className="relative flex items-center h-full rounded-lg  px-6 py-2 text-sm"
-                    style={{
-                      background:
-                        'linear-gradient(90deg, #453A3A 0%, rgba(56, 45, 46, 0.00) 100%)',
-                    }}
-                  >
-                    <FormattedMessage id="black_event_work3" />
-                  </div>
-                </div>
-              </SwiperSlide>
-            </Swiper>
-          </div>
-        </div>
-      </div>
-
-      <div className="w-full flex flex-col items-center px-2">
+      <div className="w-full flex flex-col items-center px-2 mb-5">
         <div className="flex gap-4 items-center text-xl md:text-4xl font-bold text-center">
           <img
-            src={require('@/assets/blackfriday-gift.png')}
-            className="w-[35px] md:w-[70px] h-[35px] md:h-[70px] object-cover"
+            src={require('@/assets/xmas-left.png')}
+            className="w-[35px] md:w-[74px] h-[35px] md:h-[96px] object-cover"
             alt=""
           />
-          <FormattedMessage id="black_event_title" />
+          <FormattedMessage id="event_title" />
 
           <img
-            src={require('@/assets/blackfriday-star.png')}
-            className="w-[35px] md:w-[70px] h-[35px] md:h-[70px] object-cover"
+            src={require('@/assets/xmas-right.png')}
+            className="w-[35px] md:w-[74px] h-[35px] md:h-[96px] object-cover"
             alt=""
           />
         </div>
         <p className="text-gray text-center">
-          <FormattedMessage id="black_event_desc" />
+          <FormattedMessage id="event_desc" />
         </p>
       </div>
-      <div className="relative mx-4 md:mx-0 mb-4 sm:mb-10 flex border-b border-light">
-        <div className="flex w-1/6 items-end sm:w-1/3"></div>
-        <div className="flex w-4/6 items-center sm:w-1/3">
-          <h2
-            className="row-start-1 row-end-1 -mb-px ml-auto mr-auto mt-10 border-b-[3px] border-green px-7 pb-[20px] pt-[5px] text-center text-base font-semibold uppercase text-white sm:text-lg lg:col-start-2 lg:col-end-2"
-            style={{
-              background:
-                ' var(--j-1, linear-gradient(0deg, rgba(54, 122, 46, 0.68) 0%, rgba(0, 0, 0, 0.00) 98.14%))',
-            }}
-          >
-            <div className="flex items-center text-sm sm:text-base sm:leading-[28px]">
-              <FormattedMessage id="event_label" />
-            </div>
-          </h2>
-        </div>
-        <div className="flex w-1/6 items-end sm:w-1/3"></div>
-      </div>
+
       {data && data?.length > 0 && (
         <div className="flex flex-col gap-4 mx-4 md:mx-0">
           {data?.map((item, index) => {
             return (
               <div
-                className="flex flex-col md:flex-row px-10 pb-4 justify-between items-center bg-black rounded border md:border-none border-light"
+                className="flex flex-col md:flex-row pb-4 justify-between items-center bg-black rounded border md:border-none border-light"
                 key={index}
               >
                 <div className="md:flex-1 flex flex-col md:flex-row items-center py-2 md:px-10 w-full md:w-fit bg-[url('@/assets/halloween-issue-bg.png')] bg-no-repeat bg-contain bg-center md:bg-left">
-                  <div className="w-12 md:w-24 h-12 md:h-24 overflow-hidden flex items-center">
+                  <div className="w-12 md:w-[158px] h-12 md:h-[118px] overflow-hidden flex items-center">
                     <img
-                      src={require('@/assets/blackfriday-eventBg.png')}
+                      src={require('@/assets/xmas-xiaolu.png')}
                       className="w-full object-cover"
                     />
                   </div>
                   <div className=" flex flex-col md:flex-row items-center gap-2">
-                    {item?.taskName}
-                    {renderProgress(item)}
+                    {/* {item?.taskName}
+                    {renderProgress(item)} */}
+                    123
                   </div>
                 </div>
                 <div className="flex flex-col md:flex-row items-center md:pr-16 md:gap-20">
                   <div className="text-green text-lg font-semibold">
-                    ${item?.quantity}
+                    {/* {item?.quantity ? `$${item?.quantity}` : null} */}
                   </div>
-                  {item?.reward ? (
+                  {/* {item?.reward ? (
                     <div className="w-[256px] h-[48px] bg-light rounded flex items-center justify-center  cursor-not-allowed ">
                       <FormattedMessage id="event_recieved" />
                     </div>
@@ -358,13 +196,129 @@ export default () => {
                         <FormattedMessage id="event_recieve" />
                       )}
                     </div>
-                  )}
+                  )} */}
                 </div>
               </div>
             );
           })}
         </div>
       )}
+      <div className="w-full flex justify-center">
+        <div
+          className="btn my-5"
+          style={{
+            background:
+              'var(--M1, linear-gradient(270deg, #0BFF59 0.04%, #B4FC3B 99.77%))',
+          }}
+          onClick={() => {
+            window.open('https://discord.gg/ucDMTAbAcS');
+          }}
+        >
+          <FormattedMessage id="event_xmas_lxwm" />
+        </div>
+      </div>
+      <div className="w-full  md:max-w-5xl items-center mx-auto justify-center mt-4">
+        <div
+          className="text-center text-gray cursor-pointer"
+          onClick={() => {
+            setShow(!show);
+          }}
+        >
+          <FormattedMessage id="event_xmas_tip" /> {show ? <UpOutlined /> : <DownOutlined />}
+        </div>
+        <div
+          className={`w-full overflow-y-clip mt-2  ${show ? 'show' : 'hide'}`}
+        >
+          <div className="hidden grid-cols-3 gap-12 lg:grid">
+            <div
+              className="relative flex items-center h-full rounded-lg  px-6 py-2 text-sm"
+              style={{
+                background:
+                  'linear-gradient(90deg, #453A3A 0%, rgba(56, 45, 46, 0.00) 100%)',
+              }}
+            >
+              <FormattedMessage id="event_xmas_tip1" />
+            </div>
+            <div
+              className="relative flex items-center h-full rounded-lg  px-6 py-2 text-sm"
+              style={{
+                background:
+                  'linear-gradient(90deg, #453A3A 0%, rgba(56, 45, 46, 0.00) 100%)',
+              }}
+            >
+              <FormattedMessage id="event_xmas_tip2" />
+            </div>
+            <div
+              className="relative flex items-center h-full rounded-lg  px-6 py-2 text-sm"
+              style={{
+                background:
+                  'linear-gradient(90deg, #453A3A 0%, rgba(56, 45, 46, 0.00) 100%)',
+              }}
+            >
+              <FormattedMessage id="event_xmas_tip3" />
+            </div>
+          </div>
+
+          <Swiper className="block lg:!hidden">
+            <SwiperSlide>
+              <div className="swiper-slide swiper-slide-active">
+                <div
+                  className="relative flex items-center h-full rounded-lg  px-6 py-2 text-sm"
+                  style={{
+                    background:
+                      'linear-gradient(90deg, #453A3A 0%, rgba(56, 45, 46, 0.00) 100%)',
+                  }}
+                >
+                  <FormattedMessage id="event_xmas_tip1" />
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="swiper-slide swiper-slide-next">
+                <div
+                  className="relative flex items-center h-full rounded-lg  px-6 py-2 text-sm"
+                  style={{
+                    background:
+                      'linear-gradient(90deg, #453A3A 0%, rgba(56, 45, 46, 0.00) 100%)',
+                  }}
+                >
+                  <FormattedMessage id="event_xmas_tip2" />
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="swiper-slide">
+                <div
+                  className="relative flex items-center h-full rounded-lg  px-6 py-2 text-sm"
+                  style={{
+                    background:
+                      'linear-gradient(90deg, #453A3A 0%, rgba(56, 45, 46, 0.00) 100%)',
+                  }}
+                >
+                  <FormattedMessage id="event_xmas_tip3" />
+                </div>
+              </div>
+            </SwiperSlide>
+          </Swiper>
+        </div>
+      </div>
+      {/* <div className="relative mx-4 md:mx-0 mb-4 sm:mb-10 flex border-b border-light">
+        <div className="flex w-1/6 items-end sm:w-1/3"></div>
+        <div className="flex w-4/6 items-center sm:w-1/3">
+          <h2
+            className="row-start-1 row-end-1 -mb-px ml-auto mr-auto mt-10 border-b-[3px] border-green px-7 pb-[20px] pt-[5px] text-center text-base font-semibold uppercase text-white sm:text-lg lg:col-start-2 lg:col-end-2"
+            style={{
+              background:
+                ' var(--j-1, linear-gradient(0deg, rgba(54, 122, 46, 0.68) 0%, rgba(0, 0, 0, 0.00) 98.14%))',
+            }}
+          >
+            <div className="flex items-center text-sm sm:text-base sm:leading-[28px]">
+              <FormattedMessage id="event_label" />
+            </div>
+          </h2>
+        </div>
+        <div className="flex w-1/6 items-end sm:w-1/3"></div>
+      </div> */}
     </div>
   );
 };
